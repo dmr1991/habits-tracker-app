0f86b4f7274236a8e4af8e9d05aad66e938a5c3c
Dina Morales <dinamrivera08@gmail.com>
2025-03-02 14:08:20 -0600

    Primer commit: archivos iniciales

---
+++ app.js
@@ -0,0 +1,57 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// Conexión a MongoDB
+const dbURI =
+  "mongodb+srv://dinamrivera08:IX9bftXvOt8Nh11O@cluster0.bjpkt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    console.log("Conectado a MongoDB - habitosApp");
+  })
+  .catch((err) => {
+    console.error("Error al conectar a MongoDB:", err);
+  });
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
+
+// Iniciar el servidor
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
